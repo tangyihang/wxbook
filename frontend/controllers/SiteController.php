2987d61a080d0fabaa0fd9c839f20ba44c21ff3d
tangyihang <820012136@qq.com>
2016-09-26 17:14:39 +0800

    1

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -25,7 +25,22 @@ class SiteController extends CController {
                 $keyword = trim($postObj->Content);
                 
                 if (!empty($keyword)) {
-                	$resultStr = $this->template->toMsgText($fromUsername, $toUsername, $keyword);
+                	$news = array(
+                			array(
+                					'title' => '凡人修仙传 起点中文网',
+                					'desc' => '凡人修仙传 起点中文网得desc',
+                					'pic' => 'http://qidian.qpic.cn/qdbimg/349573/107580/180',
+                					'link' => 'http://read.qidian.com/BookReader/Gyliu2kLjSQ1.aspx',
+                			),
+                			array(
+                					'title' => '凡人修仙传 笔趣阁',
+                					'desc' => '凡人修仙传 笔趣阁desc',
+                					'pic' => 'http://www.biquku.com/d/image/1/1429/1429s.jpg',
+                					'link' => 'http://www.biquku.com/1/1429/',
+                			),
+                	);
+                	$resultStr = $this->template->toMsgNews($fromUsername, $toUsername, $news);
+                	//$resultStr = $this->template->toMsgText($fromUsername, $toUsername, $keyword);
                 	echo $resultStr;
                 } else {
                 	echo "Input something...";
